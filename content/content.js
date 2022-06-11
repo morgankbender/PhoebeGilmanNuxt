3a6ba0639708df28377da3acd39284103bc9df93
Morgan <35503542+morgankbender@users.noreply.github.com>
2022-06-10 21:52:30 -0300

    init

---
+++ content/content.js
@@ -0,0 +1,44 @@
+export const MENU_ITEMS = [
+  {
+    text: 'Contact',
+    route: '/contact-us',
+    imagePath: 'assets/pigs/pig2.png',
+    imageAlt: 'Queen pig- click to visit contact page'
+  },
+  {
+    text: 'Teachers',
+    route: '/teachers',
+    imagePath: 'assets/pigs/pig4.png',
+    imageAlt: 'Lady pig- click to visit teachers page'
+  },
+  {
+    text: 'Kids',
+    route: '/kids',
+    imagePath: 'assets/pigs/pig7.png',
+    imageAlt: 'Martian pig- click to visit kids page'
+  },
+  {
+    text: 'Books',
+    route: '/books',
+    imagePath: 'assets/pigs/pig5.png',
+    imageAlt: 'Scottish pig- click to go to books page'
+  },
+  {
+    text: 'About',
+    route: '/about',
+    imagePath: 'assets/pigs/pig1.png',
+    imageAlt: 'Rose pig- click to go to about page'
+  }
+]
+
+export const BOOKS = {
+  jjTreasury: {
+    name: 'A Treasury of Jillian Jiggs',
+    about: 'This book collects all the Jillian Jiggs stories, plus extra bonus material, into one oversize hardcover.',
+    coverPath: '/assets/books/jjTreasury/cover.jpg',
+    coverAlt: 'Treasury of Jillian Jiggs cover',
+    underlinePath: '/assets/books/jjTreasury/underline.png',
+    underlineAlt: 'ribbon',
+    headerPath: '/assets/books/jjTreasury/header.jpg'
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <NuxtLink v-for="item in menuItems" :key="item.text" :to="item.route">
+      <img
+        :src="require(`../${item.imagePath}`)"
+        :alt="item.imageAlt"
+      />
+      <p>{{ item.text }}</p>
+    </NuxtLink>
+  </div>
+</template>
+
+<script>
+import { MENU_ITEMS } from '../content/content'
+
+export default {
+  name: 'IndexPage',
+  data () {
+    return {
+      menuItems: MENU_ITEMS
+    }
+  }
+}
+</script>
